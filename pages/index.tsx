fccfefbe4cb0431f0a172df55054e7a508e84685
safad <safadeldoul@gmail.com>
2023-02-20 20:13:46 +0100

    Add tweets to props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,36 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Feed from '../components/Feed'
-import Sidebar from '../components/Sidebar'
-import Widgets from '../components/Widgets'
-import { Tweet } from '../typings'
-import { fetchTweets } from '../utils/fetchTweets'
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 
-interface Props{
-  tweets: Tweet[]
+interface Props {
+  tweets: Tweet[];
 }
-const Home = ({tweets}:Props) => {
+const Home = ({ tweets }: Props) => {
   return (
     <div className="mx-auto lg:max-w-6xl max-h-screen overflow-hidden ">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
-       <main className='grid grid-cols-9'>
-        <Sidebar/>
+      <main className="grid grid-cols-9">
+        <Sidebar />
         <Feed tweets={tweets} />
-        <Widgets/>
-       </main>
-      
+        <Widgets />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-export const getServerSideProps: GetServerSideProps = async (context)=>{
+export default Home;
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const tweets = await fetchTweets();
   return {
-    props:{
-
-    }
-  }
-}
+    props: {
+      tweets,
+    },
+  };
+};
