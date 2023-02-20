f0b60d8ed3d77fc1697c67fa90fdf8d8412c1b69
safad <safadeldoul@gmail.com>
2023-02-20 17:46:12 +0100

    Add serverSide props object

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props{
+  tweets: Tweet[]
+}
+const Home = ({tweets}:Props) => {
   return (
     <div className="mx-auto lg:max-w-6xl max-h-screen overflow-hidden ">
       <Head>
@@ -13,7 +18,7 @@ const Home: NextPage = () => {
       </Head>
        <main className='grid grid-cols-9'>
         <Sidebar/>
-        <Feed />
+        <Feed tweets={tweets} />
         <Widgets/>
        </main>
       
@@ -22,3 +27,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+export const getServerSideProps: GetServerSideProps = async (context)=>{
+  const tweets = await fetchTweets();
+  return {
+    props:{
+
+    }
+  }
+}
