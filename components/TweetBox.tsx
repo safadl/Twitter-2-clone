bc0aaa02f391c01bc6bdb5c6ddc24d7e4c54d723
safad <safadeldoul@gmail.com>
2023-02-20 14:56:26 +0100

    Create new TextBox component

---
+++ components/TweetBox.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  PhotoIcon,
+  MagnifyingGlassCircleIcon,
+  FaceSmileIcon,
+  CalendarIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/outline";
+
+function TweetBox() {
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        className="h-14 w-14 object-cover rounded-full mt-4 "
+        src="https://links.papareact.com/gll"
+        alt=""
+      />
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            type="text"
+            placeholder="What's Happening?"
+            className="h-24 x-full text-xl outline-none placeholder:text-xl"
+          />
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotoIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <MagnifyingGlassCircleIcon className="h-5 w-5" />
+              <FaceSmileIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <MapPinIcon className="h-5 w-5" />
+            </div>
+            <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TweetBox;
