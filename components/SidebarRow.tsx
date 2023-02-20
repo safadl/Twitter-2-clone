926ed99f60f2ca7f3e6408ed202d31b2bb3f7b60
safad <safadeldoul@gmail.com>
2023-02-20 14:39:17 +0100

    refactor sidebar row style

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,14 +1,16 @@
 import React, { SVGProps } from "react";
 
 interface Props {
-  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element  | any;
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element | any;
   title: string;
 }
 function SidebarRow({ Icon, title }: Props) {
   return (
-    <div className=" group cursor-pointer flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 group">
+    <div className="group cursor-pointer flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 group">
       <Icon className="h-6 w-6" />
-      <p className="group-hover:text-twitter">{title}</p>
+      <p className="text-base font-light hidden group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}
+      </p>
     </div>
   );
 }
