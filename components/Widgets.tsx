d77708f2a1b85bac3cd52ee2e81ee8d4416071de
safad <safadeldoul@gmail.com>
2023-02-20 14:39:29 +0100

    Add widgets component

---
+++ components/Widgets.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
+function Widgets() {
+  return (
+    <div className="px-2 mt-2 col-span-2 hidden lg:inline">
+      <div className="mt-2 flex items-center space-x-2 bg-gray-100 p-3 rounded-full  ">
+        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 outline-none bg-transparent"
+        />
+      </div>
+       <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="coldplay"
+  options={{height: 1000}}
+/>
+    </div>
+  );
+}
+
+export default Widgets;
