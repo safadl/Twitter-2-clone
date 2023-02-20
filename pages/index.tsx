962bf6c134293746bfda369beda85c197cdf775e
safad <safadeldoul@gmail.com>
2023-02-20 14:39:47 +0100

    Add new component & style to main component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto lg:max-w-6xl max-h-screen overflow-hidden ">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
-       <main>
+       <main className='grid grid-cols-9'>
         <Sidebar/>
         <Feed />
+        <Widgets/>
        </main>
       
     </div>
