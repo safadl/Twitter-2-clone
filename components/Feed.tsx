bcae5bde12f1cc0e867f58a035d694fb8c8dc65d
safad <safadeldoul@gmail.com>
2023-02-21 18:11:52 +0100

    Add toast for showing refreshed feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import TweetBox from "./TweetBox";
 import { Tweet } from "../typings";
 import  TweetComponent from "./Tweet";
+import { fetchTweets } from "../utils/fetchTweets";
+import {toast} from 'react-hot-toast';
 interface Props{
 tweets: Tweet[]
 }
 
-function Feed({tweets}:Props) {
-   console.log(tweets)
+function Feed({tweets: tweetsProp}:Props) {
+  const [tweets,setTweets]=useState<Tweet[]>(tweetsProp)
+
+   const handleRefresh= async ()=>{
+    const refreshToast = toast.loading('Refreshing ...');
+   const tweets=await fetchTweets();
+   setTweets(tweets);
+   toast.success(' Feed Updated!',{
+    id:refreshToast
+   })
+   }
   return (
     <div className="col-span-7 lg:col-span-5 border-x">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <ArrowPathIcon
+        onClick={handleRefresh}
           className="mr-5 mt-5 h-8 w-8 curser-pointer
          text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
         />
